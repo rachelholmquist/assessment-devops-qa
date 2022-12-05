9325c4dec1cbd01182abf37f9833ab38eb2c1c64
Rachel Holmquist <rachelflorek18@outlook.com>
2022-12-05 10:52:18 -0800

    through part 3 complete

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,10 @@
 const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
-    // CODE HERE
+    test('be the same length as the length of the array passed in', () => {
+        expect(shuffleArray.length).toEqual(Array.length);
+    });
+    test('should return an array', () => {
+        expect(shuffleArray).toBe(Array);
+    })
 })
